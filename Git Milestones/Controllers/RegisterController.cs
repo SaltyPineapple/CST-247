c625a39f07f9233d6c09336572abf609164f6384
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-07-12 16:27:13 -0700

    Added Files for Registration Page

---
+++ Git Milestones/Controllers/RegisterController.cs
@@ -0,0 +1,29 @@
+﻿using Registration.Models;
+using Registration.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.WebSockets;
+
+namespace Registration.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public ActionResult Register(PlayerModel model) {
+            Security registration = new Security();
+            registration.addNewUser(model);
+            return View("RegisterPassed");
+        }
+    }
+}
---
+++ Git Milestones/Services/Security.cs
@@ -0,0 +1,45 @@
+﻿using Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Registration.Services {
+    public class Security {
+
+        SqlConnection connection = new SqlConnection("Data source = (localdb)\\MSSQLLocalDB; " +
+               "Initial Catalog=Players;Integrated Security = true");
+
+        public void addNewUser(PlayerModel model) {
+
+            try { 
+                SqlCommand cmd = new SqlCommand("AddPlayer", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@FIRSTNAME", model.FirstName);
+                cmd.Parameters.AddWithValue("@LASTNAME", model.LastName);
+                cmd.Parameters.AddWithValue("@SEX", model.Sex);
+                cmd.Parameters.AddWithValue("@AGE", model.Age);
+                cmd.Parameters.AddWithValue("@STATE", model.State);
+                cmd.Parameters.AddWithValue("@EMAIL", model.Email);
+                cmd.Parameters.AddWithValue("@USERNAME", model.Username);
+                cmd.Parameters.AddWithValue("@PASSWORD", model.Password);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+
+
+            }
+            catch(Exception ex) {
+                if(connection.State == System.Data.ConnectionState.Open) {
+                    connection.Close();
+                }
+               
+                Console.WriteLine(ex.Message.ToString());
+                
+            }
+
+        }
+    }
+}
