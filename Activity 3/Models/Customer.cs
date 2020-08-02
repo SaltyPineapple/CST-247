d44776c538437b10340e3d2e78eff9c52ec4a86a
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-08-01 19:38:01 -0700

    Adding Activity Three Project Files

---
+++ Activity 3/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity_3.Models {
+    public class Customer {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public int Age { get; set; }
+
+        public Customer(int id, string name, int age) {
+            Name = name;
+            ID = id;
+            Age = age;
+        }
+    }
+}
