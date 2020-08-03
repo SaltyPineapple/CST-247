2ebb99ed41c19b4a213d73e4719a80473ed89eec
patdalcia <49659510+patdalcia@users.noreply.github.com>
2020-08-02 20:03:40 -0700

    Migrating game rules and logic to backend service class

--- Milestone Project Files/Controllers/GameController.cs
+++ Milestone Project Files/Controllers/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using Registration.Models;
+using Registration.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace Registration.Controllers
     public class GameController : Controller
     {
         public static Board gameBoard;
+        private static GameService gService = new GameService();
 
         // GET: Game
         
@@ -19,37 +21,27 @@ namespace Registration.Controllers
             return View();
         }
 
+
         [HttpPost]
-        /*This method redirects the user to the appropriate page depending on selected board size*/
+        /*
+         * This method redirects the user to the appropriate page depending on selected board size
+         * 
+         */
         public ActionResult submitGameDetails(Board m)
         {
-            //Creating board and sizing it based on user choices
+            //using game servie to set up game board
             gameBoard = new Board();
+            gameBoard = gService.SetUpBoard(m);
 
-            m.setSizeFromRadio(m.size);
-            m.setUpLiveNeighbors();
-            m.calculateLiveNeighbors();
-
-            gameBoard = m;
-
-            //Redirecting user to appropriate game board
-            switch (m.size)
-            {
-                case "small":
-                    return View("PlaySmall", m);
-                    break;
-                case "medium":
-                    return View("PlayMedium", m);
-                    break;
-                case "large":
-                    return View("PlayLarge", m);
-                    break;
-                default:
-                    return View("Index");
-            }
+            //Using game service to return appropriate gameBoard based on size
+            return View(gService.DirectToGame(gameBoard.size), gameBoard);
         }
 
-        //This method handles button clicks for the game board
+
+        /*
+         * This method handles button clicks for the game board
+         * 
+         */
         public ActionResult onButtonClick(String gameButtonValue)
         {
             //Geting location of selected button and storing within local variables
@@ -60,80 +52,28 @@ namespace Registration.Controllers
             //Testing if selected cell is live
             if (gameBoard.grid[x, y].live)
             {
-                finishGame();
+                gameBoard.win = gService.finishGame(gameBoard);
             }
        
             //Calling flood fill to open neighboring cells with zero live neighbors
             gameBoard.floodFill(x, y);
 
             //Checking if win conditions are met
-            if (checkWin())
+            if (gService.checkWin(gameBoard) || gameBoard.win == "false")
             {
-                finishGame();
-            }
-
-            //Redirecting user back to game board
-            switch (gameBoard.size)
-            {
-                case "small":
-                    return View("PlaySmall", gameBoard);
-                    break;
-                case "medium":
-                    return View("PlayMedium", gameBoard);
-                    break;
-                case "large":
-                    return View("PlayLarge", gameBoard);
-                    break;
-                default:
-                    return View("Index");
-            }
-        }
-
-        /*This method checks if win conditions have been met*/
-        public bool checkWin()
-        {
-            int numBombs = 0;
-            int unvisitedCells = 0;
-
-            for (int i = 0; i < gameBoard.length; i++)
-            {
-                for (int j = 0; j < gameBoard.width; j++)
+                gameBoard.win = gService.finishGame(gameBoard);
+                if(gameBoard.win == "true")
                 {
-                    if (!gameBoard.grid[i, j].visited)
-                    {
-                        unvisitedCells++;
-                    }
-                    if (gameBoard.grid[i, j].live)
-                    {
-                        numBombs++;
-                    }
+                    TempData["alertMessage"] = "Congratulations You have Won!";
                 }
-            }
+                else if(gameBoard.win == "false")
+                {
+                    TempData["alertMessage"] = "Oh No! You've Exploded And Lost!!";
+                }
+            }            
 
-            /*If spaces left unvisited are equal to amount of bombs*/
-            if (numBombs == unvisitedCells)
-            {
-                return true;//Win
-            }
-            else { return false; }//Not win
-        }
-
-        /*This method wraps up the game by displaying full game board and win message */
-        public void finishGame()
-        {
-            //Flag used to ensure win conditions are met
-            bool flag = checkWin();
-
-            if (flag)//win
-            {
-                gameBoard.win = "true";
-                TempData["alertMessage"] = "Congratulations You have Won!";
-            }
-            else//loss
-            {
-                gameBoard.win = "false";
-                TempData["alertMessage"] = "Oh No! You've Exploded And Lost!!";
-            }
+            //Using game service to return appropriate gameBoard based on size
+            return View(gService.DirectToGame(gameBoard.size), gameBoard);
         }
 
     }
---
+++ Milestone Project Files/Services/GameService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Registration.Models;
+
+namespace Registration.Services
+{
+    public class GameService
+    {
+        
+        public String DirectToGame(String size)
+        {
+            //Redirecting user back to game board
+            switch (size)
+            {
+                case "small":
+                    return "PlaySmall";
+                    break;
+                case "medium":
+                    return "PlayMedium";
+                    break;
+                case "large":
+                    return "PlayLarge";
+                    break;
+                default:
+                    return "Index";
+            }
+        }
+
+        public Board SetUpBoard(Board b)
+        {
+            b.setSizeFromRadio(b.size);
+            b.setUpLiveNeighbors();
+            b.calculateLiveNeighbors();
+
+            return b;
+        }
+
+        public bool checkWin(Board gameBoard)
+        {
+            int numBombs = 0;
+            int unvisitedCells = 0;
+
+            for (int i = 0; i < gameBoard.length; i++)
+            {
+                for (int j = 0; j < gameBoard.width; j++)
+                {
+                    if (!gameBoard.grid[i, j].visited)
+                    {
+                        unvisitedCells++;
+                    }
+                    if (gameBoard.grid[i, j].live)
+                    {
+                        numBombs++;
+                    }
+                }
+            }
+
+            /*If spaces left unvisited are equal to amount of bombs*/
+            if (numBombs == unvisitedCells)
+            {
+                return true;//Win
+            }
+            else { return false; }//Not win
+        }
+
+        /*
+         * This method wraps up the game by displaying full game board and win message 
+         * 
+         */
+        public string finishGame(Board gameBoard)
+        {
+            //Flag used to ensure win conditions are met
+            bool flag = checkWin(gameBoard);
+
+            if (flag)//win
+            {
+                return "true";
+            }
+            else//loss
+            {
+ 
+                return "false";
+            }
+        }
+
+    }
+}
