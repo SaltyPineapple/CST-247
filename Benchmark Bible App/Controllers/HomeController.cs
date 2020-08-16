39ee1881f31e0365fa79bbdf8486cedbd76fd4eb
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-08-16 11:38:26 -0700

    Add files via upload

---
+++ Benchmark Bible App/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/* Mark Pratt
+ * 8/12/2020
+ */
+
+namespace BibleVerseSearchApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Method: OnButtonClick
+        // gets which button was pressed on home screen and returns the view accordingly
+
+        [HttpPost]
+        public ActionResult OnButtonClick(string buttonValue) {
+            if (buttonValue == "search") {
+                return View("~/Views/Search/Search.cshtml");
+            }
+            else if (buttonValue == "entry") {
+                return View("~/Views/Entry/Entry.cshtml");
+            }
+            else {
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Benchmark Bible App/Models/Verse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BibleVerseSearchApp.Models {
+    public class Verse {
+
+        [DisplayName("Testament: ")]
+        [Required(ErrorMessage = "The Testament is required for entry.")]
+        [MaxLength(3, ErrorMessage = "Can only enter 'Old' or 'New'.")]
+        public string Testament { get; set; }
+
+        
+        [DisplayName("Book: ")]
+        [Required(ErrorMessage = "The Book is required for entry.")]
+        public string Book { get; set; }
+
+        [DisplayName("Chapter Number: ")]
+        [Required(ErrorMessage = "The chapter number is required for entry.")]
+        public int Chapter { get; set; }
+
+        [DisplayName("Verse Number: ")]
+        [Required(ErrorMessage = "The verse number is required for entry.")]
+        public int VerseNumber { get; set; }
+
+        [DisplayName("Bible Verse: ")]
+        [Required(ErrorMessage = "The actual Verse is required for entry.")]
+        [MinLength(9, ErrorMessage = "Verse has to be of an actual length.")]
+        public string BibleVerse { get; set; }
+    }
+}
