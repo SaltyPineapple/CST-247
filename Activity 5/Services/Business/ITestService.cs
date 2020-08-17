0b376edd8b8aea84c9ba2c0e014da4c971e41c06
Mark Pratt <32585200+SaltyPineapple@users.noreply.github.com>
2020-08-16 20:30:26 -0700

    Uploading Activity 5 Files

---
+++ Activity 5/Services/Business/ITestService.cs
@@ -0,0 +1,23 @@
+﻿using Activity1Part3.Services.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace Activity1Part3.Services.Business {
+    interface ITestService {
+
+        private ILogger logger;
+
+        [InjectionMethod]
+        public void Initialize(ILogger logger) {
+            this.logger = logger;
+        }
+
+        public void TestLogger() {
+            logger.Info("Test Logging in TestService.TestLogger() invoked");
+        }
+    }
+}
---
+++ Activity 5/Services/Business/TestService.cs
@@ -0,0 +1,13 @@
+﻿using Activity1Part3.Services.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity1Part3.Services.Business {
+    public class TestService : ITestService {
+
+        private ILogger logger;
+
+    }
+}
